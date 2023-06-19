afe441ee3ab9c8b2664e47e9be6befa375e1f170
Federico Ghigini <federico.ghigini@gmail.com>
2023-06-19 09:11:59 +0200

    Aggiunto accelerazione e frizione al movimento

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const ACCELERATION = 10
+const MAX_SPEED = 100
+const FRICTION = 10
+
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
@@ -7,4 +11,15 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	move_and_collide(input_vector)
+	# impedisce di muoversi più velocemente sulle diagonali
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	print(velocity)
+	
+	move_and_collide(velocity)
